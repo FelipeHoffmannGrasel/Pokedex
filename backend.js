f8998a4d9234dcb85f819a063d6888f9eb0d20eb
FelipeHoffmann <felipegrasel2@gmail.com>
2023-12-12 17:41:46 -0300

    alterações finais

--- backend.js
+++ backend.js
@@ -48,28 +48,8 @@ function addPokemonToPokedex(id, nome, front_img, back_img, types, stats, moves)
     pokedex_list.sort((a, b) => a.id - b.id);
 }
 
-function details_data(pokemon) {
+function details_data(pokemon, pokemons_list, pokedex_list) {
     openDetailSideNav()
-
-// case "fire": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-yx7Ts5Om1oc/YF6HjOUhphI/AAAAAAAARsY/ZtISgJqMMYQqxjpX30cXpuC-XWrFQNCegCLcBGAsYHQ/s16000/capa-fogo.png')"; break;
-// case "grass": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-v3NGm_RBCUo/YF59xsFke0I/AAAAAAAARro/39dQg06EsGoevbcqk35C1PvjOxb2h-fJACLcBGAsYHQ/w1600/capa-planta.png')"; break;
-// case "electric": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-opKwujwn_6s/YE6ucWtSYZI/AAAAAAAARaw/tIn7NkfMD8QIrCydeC9T40HUDqJaTjYTgCLcBGAsYHQ/s16000/pokemon-tipo-eletrico.png')"; break;
-// case "water": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-KieHXRLic1U/YF6EuV7RPoI/AAAAAAAARsI/8NZOovxMOIQCSSkiNoRMbfm_FjObDefJQCLcBGAsYHQ/s16000/capa-agua.png')"; break;
-// case "ground": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-txY30VfqDnk/YF6BcYLyrbI/AAAAAAAARr4/yt7ZF-zgEaQyhPwnW3HWVvmlO1c3CLdPACLcBGAsYHQ/s16000/capa-terrestre.png')"; break;
-// case "rock": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-4ovl8Je905g/YFqQMU8_ZxI/AAAAAAAARpE/qFatY5vc2fIDEtqBA14NRsQ-fUXgShp-wCLcBGAsYHQ/s16000/capa-pedra.png')"; break;
-// case "fairy": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-DMO3sFcaMjg/YFf0FWgfrfI/AAAAAAAARno/EtwYBQkUNzw1idLf8viKBTTeJCNGBoXZwCLcBGAsYHQ/s16000/capa-fada.png')"; break;
-// case "poison": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-iPYs5JUk3SY/YFkpfJbEBNI/AAAAAAAARoA/9WBdGCZBT08zkPXZjj0Hiao-MSww7lphwCLcBGAsYHQ/s16000/capa-venenoso.png')"; break;
-// case "bug": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-wXdSSoxscp8/YFkxtNTVnxI/AAAAAAAARoQ/AHbeYQF09oAWLycmErpvAzEsTaOGM5m2QCLcBGAsYHQ/s16000/capa-inseto.png')"; break;
-// case "dragon": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-HKqr1yDr2rA/YElb41nv6xI/AAAAAAAARZ8/c-AVvfM4PEAx3lAj1ZFbaMjg0TrxZhFzwCLcBGAsYHQ/s16000/capa-drag%25C3%25A3o.png')"; break;
-// case "psychic": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-CUPfS9zcUd4/YFkuv73DqmI/AAAAAAAARoI/epjrslcw2D8vjAJh3YzOZ7YrNIfTmXd2ACLcBGAsYHQ/s16000/capa-ps%25C3%25ADquico.png')"; break;
-// case "flying": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-5CYmRmeZ9lU/YF56TYa6f1I/AAAAAAAARrg/yQivVqEfkVcUlOhn0bJv9KIcnVceywJ7gCLcBGAsYHQ/s16000/capa-voador.png')"; break;
-// case "fighting": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-NeXaV_qBXt4/YFlCLptBOoI/AAAAAAAARow/RPL-VcKvpSwNBZoyVM6cm17JLh04Qk-1wCLcBGAsYHQ/s16000/capa-lutador.png')"; break;
-// case "normal": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/--f7Jcmtmv0Q/YFqMvVdzBRI/AAAAAAAARo8/z-QQS6M3Lvwfnb1uMH2dRSWGIWhiozAXACLcBGAsYHQ/s16000/capa-normal.png')"; break;
-// case "ghost": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-eALvZG55jgo/YFfuzWOS63I/AAAAAAAARng/PDhMZoMa-84uL3Yjcca_yeDB5TrdSCSTACLcBGAsYHQ/s16000/capa-fantasma.png')"; break;
-// case "steel": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-RXlmlQNKRO0/YFk-RYq4PWI/AAAAAAAARoo/FRsmm1_0xUkv_CdHX-lhfDncEgFyAb7eACLcBGAsYHQ/s16000/capa-a%25C3%25A7o.png')"; break;
-// case "dark": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-jVegaHym89M/YFk1fXN5XUI/AAAAAAAARoY/WIcQsPM3kZ8FEIf0Shyfz-qdDlf59N4vQCLcBGAsYHQ/s16000/capa-sombrio.png')"; break;
-// case "ice": poke_div.style.backgroundImage = "url('https://1.bp.blogspot.com/-d_zGMNy_vCE/YFk4md636HI/AAAAAAAARog/xNLHR_oVviE8lpjioFetIpIURUUzYDeIwCLcBGAsYHQ/s16000/capa-gelo.png')"; break;
-
 
     // cor de fundo da pagina detalhes
     switch (pokemon.types[0]) {
@@ -103,7 +83,7 @@ function details_data(pokemon) {
     // for pra criar a lista de status // max 5 status na list
     for (let i = 0; i < 5; i++) {
         let status = document.createElement("p"); status.id = "div_status"
-        status.innerHTML = `${pokemon.stats[i].nome}`
+        status.innerHTML = `${pokemon.stats[i].nome}: ${pokemon.stats[i].value}`
         document.getElementById("details_list").appendChild(status)
     }
 
@@ -122,6 +102,61 @@ function details_data(pokemon) {
         let move = document.createElement("p"); move.id = "move_p"
         move.innerHTML = `${pokemon.moves[i]}`
         document.getElementById("moves").appendChild(move)
+    }
+
+    // botão add/remove da pokedex
+    let add_remove_btn = document.getElementById("add_to_pokedex_btn");
+    let isInPokedex = pokedex_list.some(p => p.id === pokemon.id);
+
+    if (isInPokedex) {
+        add_remove_btn.innerText = `Remover ${pokemon.nome} da pokedex`;
+        add_remove_btn.onclick = function () {
+            removeFromPokedex(pokemon);
+        };
+    } else {
+        add_remove_btn.innerText = `Adicionar ${pokemon.nome} na pokedex`;
+        add_remove_btn.onclick = function () {
+            addToPokedex(pokemon);
+            hideFromHomePage(pokemon); // Esconde o Pokémon da página inicial após ser adicionado à Pokédex
+        };
+    }
+}
+
+function removeFromPokedex(pokemon) {
+    let index = pokedex_list.findIndex(p => p.id === pokemon.id);
+    if (index !== -1) {
+        let removedPokemon = pokedex_list.splice(index, 1)[0];
+        showOnHomePage(removedPokemon); // Mostra o Pokémon na página inicial novamente
+    }
+    hideFromPokedexPage(pokemon); // Esconde o Pokémon da página da Pokédex
+}
+
+function showOnHomePage(pokemon) {
+    let pokemonElement = document.getElementById(`pokemon_${pokemon.id}`);
+    if (pokemonElement) {
+        pokemonElement.style.display = 'inline'; // Exibe o Pokémon na página inicial
+    }
+}
+
+function addToPokedex(pokemon) {
+    let isAlreadyInPokedex = pokedex_list.some(p => p.id === pokemon.id);
+    if (!isAlreadyInPokedex) {
+        pokedex_list.push(pokemon);
+        hideFromHomePage(pokemon);
+    }
+}
+
+function hideFromHomePage(pokemon) {
+    let pokemonElement = document.getElementById(`pokemon_${pokemon.id}`);
+    if (pokemonElement) {
+        pokemonElement.style.display = 'none'; // Esconde o Pokémon da página inicial
+    }
+}
+
+function hideFromPokedexPage(pokemon) {
+    let pokedexElement = document.getElementById(`pokemon_${pokemon.nome}`);
+    if (pokedexElement) {
+        pokedexElement.style.display = 'none'; // Esconde o Pokémon da página da Pokédex
     }
 }
 
@@ -159,14 +194,14 @@ function run_fetch() {
                 fetch(data.url)
                     .then(response => response.json())
                     .then(function (pokemon) {
-                        // console.log(pokemon)
+                        console.log(pokemon)
 
                         let id = pokemon.id;
                         let nome_pokemon = pokemon.name;
                         let front_img = pokemon.sprites.versions['generation-v']['black-white'].animated.front_default;
                         let back_img = pokemon.sprites.versions['generation-v']['black-white'].animated.back_default;
                         let types = pokemon.types.map(type => type.type.name);
-                        let stats = pokemon.stats.map(stat => { return { nome: stat.stat.name } })
+                        let stats = pokemon.stats.map(stat => { return { nome: stat.stat.name, value: stat.base_stat } })
                         let moves = pokemon.moves.slice(0, 5).map(move => move.move.name)
 
                         addPokemonToArray(
@@ -192,8 +227,6 @@ function run_fetch() {
 
 run_fetch();
 showPokemons = function () {
-
-    // console.log(pokemons_list);
     pokemons_list.forEach(pokemon => {
         //////////////// pagina inicial /////////////////
 
@@ -237,7 +270,7 @@ showPokemons = function () {
         details.innerText = `Detalhes`;
         
         details.addEventListener('click', () => {
-            details_data(pokemon)
+            details_data(pokemon, pokemons_list, pokedex_list)
         })
 
         ////////////// pagina pokedex //////////////////
@@ -259,7 +292,6 @@ showPokemons = function () {
             
             // faz o mesmo pokemon adicionado sumir da página inicial
             document.getElementById(`pokemon_${pokemon.id}`).style.display = "None"
-            console.log(pokedex_list)
         }
 
         // quando clica pra entrar na pokedex
@@ -293,7 +325,7 @@ showPokemons = function () {
                 // quando clica no botão de detalhes
                 detalhes.addEventListener('click', () => {
                     closePokedexSideNav()
-                    details_data(pokemon)
+                    details_data(pokemon, pokemons_list, pokedex_list)
 
                     // quando já está na pokedex e clica nos detalhes, quando fecha volta pra pokedex e não pra pagina inicial
                     document.getElementById('detail_closebtn').onclick = function() {
@@ -307,9 +339,9 @@ showPokemons = function () {
                     let container = document.getElementById('pokedex_div');
                     container.removeChild(divToRemove);
 
-                    if (pokedex_list.indexOf(pokemon) !== -1) {
-                        pokedex_list.splice(pokedex_list.indexOf(pokemon), 1);
-                    }
+                    console.log(pokedex_list)
+                    pokedex_list.pop(pokemon)
+                    console.log(pokedex_list)
                     document.getElementById(`pokemon_${pokemon.id}`).style.display = "inline";
 
                     addPokemonToArray(
